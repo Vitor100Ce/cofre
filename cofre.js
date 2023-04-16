39aabeff71b60fb05d8cc2dd1449013c4ebf9eb3
Vitor100Ce <88120850+Vitor100Ce@users.noreply.github.com>
2023-04-16 12:12:33 -0300

    cofre

---
+++ cofre.js
@@ -0,0 +1,31 @@
+
+function processData(input){
+
+
+    const separaDados = input.trim().split(' ');
+    const senhaCorreta = separaDados[0];
+    const senhaDigitada = separaDados[1];
+
+    const senhaDigitadaSanitizada = senhaDigitada.split('').reduce(
+      (acumulador, valorAtual) => {
+        const letraSenhaCorreta = senhaCorreta[acumulador.length]
+        if(valorAtual === letraSenhaCorreta) {
+            return acumulador + valorAtual
+        }
+    
+        return acumulador
+      },'');
+    
+    if(senhaDigitadaSanitizada === senhaCorreta){
+        console.log('SIM');
+    } else{
+        console.log('NAO');
+    }
+
+    console.log(senhaDigitada)
+
+}
+
+
+
+processData('cubos cuggbyos');
